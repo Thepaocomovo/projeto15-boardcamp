ab4bea0535d8312210bff2448eeeb2221e885162
wesley <wesleysantos20012001@gmail.com>
2022-10-02 22:33:23 -0300

    feat[rentals/route] creation of simple GET method on rentals route without the paramos options

--- src/controllers/Rentals.controller.js
+++ src/controllers/Rentals.controller.js
@@ -6,25 +6,79 @@ import connection from "../database/PgConnection.js";
 
 const getRentals = async (req, res) => {
 
-    res.sendStatus(StatusCodes.OK);
+
+
+
+
+
+    try {
+        let rentalsList = await connection.query(`
+            SELECT 
+                rentals.*, 
+                customers.name, 
+                games.name as "gamesName", games."categoryId", 
+                categories.name as "categoryName"
+            FROM rentals
+            JOIN customers
+            ON rentals."gameId" = customers.id
+            JOIN games
+            ON rentals."gameId" = games.id
+            JOIN categories
+            ON games."categoryId" = categories.id;
+        `)
+
+        rentalsList = rentalsList.rows.map((v) => {
+            return (
+                {
+                    id: v.id,
+                    customerId: v.customerId,
+                    gameId: v.gameId,
+                    rentDate: v.rentDate,
+                    daysRented: v.daysRented,
+                    returnDate: v.returnDate,
+                    originalPrice: v.originalPrice,
+                    delayFee: v.delayFee,
+                    customer: {
+                        id: v.customerId,
+                        name: v.name
+                    },
+                    game: {
+                        id: v.gameId,
+                        name: v.gamesName,
+                        categoryId: v.categoryId,
+                        categoryName: v.categoryName
+                    }
+                }
+            )
+        })
+
+        return res.status(StatusCodes.OK).send(rentalsList);
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
+    }
 };
 
 const createRental = async (req, res) => {
     const { customerId, gameId, daysRented } = req.body;
     const time = dayjs(Date()).format('YYYY-MM-DD');
-   const originalPrice = res.locals.originalPrice;
+    console.log(time);
+    console.log(typeof (time))
+    const originalPrice = res.locals.originalPrice;
 
     if (isNaN(Number(daysRented)) || daysRented < 1) {
         return res.sendStatus(StatusCodes.BAD_REQUEST);
     }
 
     try {
+        console.log(time);
         await connection.query(`
             INSERT INTO rentals
             ("customerId", "gameId", "daysRented", "rentDate", "returnDate", "originalPrice", "delayFee")
             VALUES ($1, $2, $3, $4, $5, $6, $7);
         `, [customerId, gameId, daysRented, time, null, originalPrice, null]
-        ) 
+        )
+        console.log(time);
     } catch (error) {
         console.log(error);
         return res.sendStatus(StatusCodes.INTERNAL_SERVER_ERROR);
